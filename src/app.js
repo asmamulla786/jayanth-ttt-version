a01a8a8f3d3fa1bed1c65a35bb5fb6194c554225
Srijayanth Sridhar <cr4tyb0n3s@gmail.com>
2025-04-05 17:55:39 +0530

    Adds basic handler for /

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
+import { getCookie } from 'hono/cookie';
 
-export const createApp = () => {
+const addSessions = (sessions) => async (c, next) => {
+  c.set('sessions', sessions);
+  return await next();
+}
+
+const serveHome = (c) => {
+  const sessionId = getCookie(c, 'sessionId');
+  const sessions = c.get('sessions');
+  if (sessionId && sessions.has(sessionId)) {
+    return c.redirect('/home', 303);
+  }
+  return c.redirect('/login', 303);
+}
+
+export const createApp = (sessions) => {
   const app = new Hono();
+  app.use(addSessions(sessions));
   app.use('*', logger());
-  app.get('/', c => c.text('Hello World!'));
+  app.get('/', serveHome);
+
   return app;
 };
--- test/app_test.js
+++ test/app_test.js
@@ -1,13 +1,43 @@
 import { assertEquals } from 'jsr:@std/assert';
-import { describe, it } from 'jsr:@std/testing/bdd';
+import { describe, test, it } from 'jsr:@std/testing/bdd';
 import { createApp } from '../src/app.js';
 
-describe('App', () => {
-  it('should respond with Hello World!', async () => {
-    const app = createApp();
-    const response = await app.request('/');
-    assertEquals(response.status, 200);
-    const text = await response.text();
-    assertEquals(text, 'Hello World!');
+describe("App", () => {
+  describe('/', () => {
+    it('should redirect to login page for an unsigned user', async () => {
+      const app = createApp();
+      const response = await app.request('/');
+      assertEquals(response.status, 303);
+      assertEquals(response.headers.get('location'), '/login');
+    });
+
+    it('should redirect to game page for a valid user', async () => {
+      const sessions = new Map();
+      sessions.set('1', { name: 'Nobody' });
+      const app = createApp(sessions);
+      const r = new Request('http://localhost/', {
+        method: 'GET',
+        headers: {
+          'Cookie': 'sessionId=1',
+        },
+      });
+      const response = await app.request(r);
+      assertEquals(response.status, 303);
+      assertEquals(response.headers.get('location'), '/home');
+    });
+
+    it('should redirect to login page for an invalid user', async () => {
+      const sessions = new Map();
+      const app = createApp(sessions);
+      const r = new Request('http://localhost/', {
+        method: 'GET',
+        headers: {
+          'Cookie': 'sessionId=99',
+        },
+      });
+      const response = await app.request(r);
+      assertEquals(response.status, 303);
+      assertEquals(response.headers.get('location'), '/login');
+    });
   });
-});
+})
